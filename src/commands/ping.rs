77038b858f43ef3a1af9cd59dab45f97b64517ac
Joël Kuijper <jmkuijper@outlook.com>
2023-10-21 16:26:00 +0200

    feat: got it working

---
+++ src/commands/ping.rs
@@ -0,0 +1,17 @@
+use crate::{Context, Error};
+
+/// Pong!
+#[poise::command(slash_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    let message = ctx.reply("Pong!").await?;
+
+    let timestamp = message.message().await?.timestamp.timestamp_millis();
+    let other_timestamp = ctx.created_at().timestamp_millis();
+
+    let ping = timestamp - other_timestamp;
+    let reply = format!("Pong! `{}ms`", ping);
+
+    message.edit(ctx, |m| m.content(reply)).await?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+mod commands;
+
+use anyhow::Context as _;
+use poise::{serenity_prelude as serenity, Event};
+use rustrict::CensorStr;
+use shuttle_poise::ShuttlePoise;
+use shuttle_secrets::SecretStore;
+use sqlx::PgPool;
+
+use crate::commands::ping::*;
+
+pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[poise::command(slash_command, prefix_command)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+async fn hello(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("world!").await?;
+    Ok(())
+}
+
+#[shuttle_runtime::main]
+async fn poise(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> ShuttlePoise<Data, Error> {
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(shuttle_runtime::CustomError::new)?;
+
+    // Get the discord token set in `Secrets.toml`
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            event_handler: |_ctx, event, _framework, _data| {
+                Box::pin(event_handler(_ctx, event, _framework))
+            },
+            commands: vec![hello(), ping(), register()],
+            ..Default::default()
+        })
+        .token(discord_token)
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build()
+        .await
+        .map_err(shuttle_runtime::CustomError::new)?;
+
+    Ok(framework.into())
+}
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &Event<'_>,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+) -> Result<(), Error> {
+    match event {
+        Event::Ready { data_about_bot } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        }
+        Event::Message { new_message } => {
+            if new_message.content.is_inappropriate() {
+                new_message
+                    .reply(ctx, "Ga je mond wassen! 🧼")
+                    .await
+                    .expect("Failed to reply to message");
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
